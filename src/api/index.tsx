a2a4cf35e332fd625ae27a8d81373591412b8e14
developjik <developjik@gmail.com>
2022-02-25 15:13:53 +0900

    chore: pull

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const proxy = 'https://cors-anywhere.herokuapp.com/';
-// const BASE_URL = 'https://storage-fe.fastraffic.io/homeworks/links';
+// const BASE_URL = 'https://app.cors.bridged.cc/ko/https://storage-fe.fastraffic.io/homeworks/links';
+
 const BASE_URL = 'https://week4-proxy.herokuapp.com/api';
 
 const getAPI = async () => {
